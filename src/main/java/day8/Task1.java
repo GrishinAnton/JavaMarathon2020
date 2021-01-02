20ef5df47b2893626ec03a6fe923bcf3fab4f824
GrishinAnton <grishin_rostov@mail.ru>
2021-01-02 10:15:12 +0300

    complete day8 tasks

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,28 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        String s = "";
+        long start = System.currentTimeMillis();
 
+        for (int i = 0; i < 20000; i++) {
+            s += i + " ";
+        }
+        long finish = System.currentTimeMillis();
+        long timeConsumedMillis = finish - start;
+
+        System.out.println(s);
+        System.out.println(timeConsumedMillis);
+
+        StringBuilder s2 = new StringBuilder("");
+        long start2 = System.currentTimeMillis();
+
+        for (int i = 0; i < 20000; i++) {
+            s2.append(i + " ");
+        }
+        long finish2 = System.currentTimeMillis();
+        long timeConsumedMillis2 = finish2 - start2;
+
+        System.out.println(s2);
+        System.out.println(timeConsumedMillis2);
     }
 }
