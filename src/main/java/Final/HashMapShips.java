31bcd8758f9b1fb3ab4d4e5a46b44c72dc691791
GrishinAnton <grishin_rostov@mail.ru>
2021-01-27 22:33:46 +0300

    Доработка

--- src/main/java/Final/HashMapShips.java
+++ src/main/java/Final/HashMapShips.java
@@ -27,25 +27,3 @@ public class HashMapShips {
         }
     }
 }
-//    Map<String, Integer> hashMap = new HashMap<>();
-//
-//// Помещаем данные на карточку
-//        hashMap.put("Васька", 5);
-//                hashMap.put("Мурзик", 8);
-//                hashMap.put("Рыжик", 12);
-//                hashMap.put("Барсик", 5);
-//
-//// Получаем набор элементов
-//                Set<Map.Entry<String, Integer>> set = hashMap.entrySet();
-//
-//// Отобразим набор
-//        for (Map.Entry<String, Integer> me : set) {
-//        System.out.print(me.getKey() + ": ");
-//        System.out.println(me.getValue());
-//        }
-//
-//// Добавляем значение
-//        int value = hashMap.get("Рыжик");
-//        hashMap.put("Рыжик", value + 3);
-//        System.out.println("У Рыжика стало " + hashMap.get("Рыжик"));
-
